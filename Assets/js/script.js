a246d333f0e46b9b0da9f3132d89ba466c27ff48
Jonathan Cushing <firex42069@gmail.com>
2022-04-10 01:10:03 -0400

    make weather data look somewhat decent

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,5 @@
-var apiKey = "4" + "8" + "0" + "6" + "b" + "8" + "0" + "a" + "8" + "7" + "d" + "4" + "d" + "5" + "f" + "0" + "3" + "2" + "1" + "3" + "f" + "3" + "9" + "8" + "1" + "b" + "4" + "7" + "9" + "9" + "a" + "d";
-var weatherEl = document.querySelector("#weather-container");
+var JECApiKey = "4" + "8" + "0" + "6" + "b" + "8" + "0" + "a" + "8" + "7" + "d" + "4" + "d" + "5" + "f" + "0" + "3" + "2" + "1" + "3" + "f" + "3" + "9" + "8" + "1" + "b" + "4" + "7" + "9" + "9" + "a" + "d";
+var weatherSectionEl = document.querySelector("#weather-container");
 var formEl = document.querySelector("form");
 var homeInputEl = document.querySelector("#home");
 var destinationInputEl = document.querySelector("#destination");
@@ -9,27 +9,27 @@ var destinationInputEl = document.querySelector("#destination");
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
-    weatherEl.innerHTML = "";
+    weatherSectionEl.innerHTML = "";
 
     var home = homeInputEl.value.trim();
     var destination = destinationInputEl.value.trim();
 
     if(!home || !destination) {
         if(!home && destination) {
-            weatherEl.innerHTML = "<h3 class='alert'>Please enter your starting location</h3>";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>Please enter your starting location</h3>";
         } else if(home && !destination) {
-            weatherEl.innerHTML = "<h3 class='alert'>Please enter your destination</h3>";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>Please enter your destination</h3>";
         } else {
-            weatherEl.innerHTML = "<h3 class='alert'>Please enter your starting location and destination</h3>";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>Please enter your starting location and destination</h3>";
         }
     } else if(home === destination) {
-        weatherEl.innerHTML = "<h3 class='alert'>Please enter different values for your starting location and destination</h3>";
+        weatherSectionEl.innerHTML = "<h3 class='alert'>Please enter different values for your starting location and destination</h3>";
     } else {
-        getLocation(destination);
-        destinationInputEl.value = "";
-        
         getLocation(home);
         homeInputEl.value = "";
+
+        getLocation(destination);
+        destinationInputEl.value = "";
     }
 
     /*var saveHome = true;
@@ -60,7 +60,7 @@ var formSubmitHandler = function(event) {
 
         displaySearchHistory();
     } else {
-        weatherEl.innerHTML = "<h3 class='alert'>Please enter a city</h3>";
+        weatherSectionEl.innerHTML = "<h3 class='alert'>Please enter a city</h3>";
     }*/
 };
 
@@ -68,20 +68,20 @@ var formSubmitHandler = function(event) {
     var targetEl = event.target;
 
     if(targetEl.matches("button")) {
-        weatherEl.innerHTML = "";
+        weatherSectionEl.innerHTML = "";
 
         var home = targetEl.textContent;
 
         if(home) {
             getLocation(home);
         } else {
-            weatherEl.innerHTML = "<h3 class='alert'>If you see this text, you've just encountered a bug that may need to be fixed. <a href='https://github.com/JEC6789/weather-dashboard/issues' target='_blank'>Please report this issue on GitHub</a> so I can look into it further.";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>If you see this text, you've just encountered a bug that may need to be fixed. <a href='https://github.com/JEC6789/weather-dashboard/issues' target='_blank'>Please report this issue on GitHub</a> so I can look into it further.";
         }
     }
 };*/
 
 var getLocation = function(city) {
-    var geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + apiKey;
+    var geoApiUrl = "https://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + JECApiKey;
 
     fetch(geoApiUrl).then(function(response) {
         if(response.ok) {
@@ -89,21 +89,22 @@ var getLocation = function(city) {
                 getWeatherData(geoData);
             });
         } else {
-            weatherEl.innerHTML = "<h3 class='alert'>Somebody just got diagnosed with skill issue. Could be you, could be me, could be the API I'm getting the data you requested from. It doesn't matter who has skill issue in the end though, as it is a disease that stops everything in its tracks. Maybe try submitting that city again and see if it changes anything.</h3>";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>Somebody just got diagnosed with skill issue. Could be you, could be me, could be the API I'm getting the data you requested from. It doesn't matter who has skill issue in the end though, as it is a disease that stops everything in its tracks. Maybe try submitting that city again and see if it changes anything.</h3>";
         }
     });
 };
 
 var getWeatherData = function(geoData) {
-    var oneCallApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geoData[0].lat + "&lon=" + geoData[0].lon + "&units=imperial&appid=" + apiKey;
+    var oneCallApiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + geoData[0].lat + "&lon=" + geoData[0].lon + "&units=imperial&appid=" + JECApiKey;
 
     fetch(oneCallApiUrl).then(function(response) {
         if(response.ok) {
             response.json().then(function(weatherData) {
+                console.log(weatherData);
                 displayWeatherData(geoData, weatherData);
             });
         } else {
-            weatherEl.innerHTML = "<h3 class='alert'>Somebody just got diagnosed with skill issue. Could be you, could be me, could be the API I'm getting the data you requested from. It doesn't matter who has skill issue in the end though, as it is a disease that stops everything in its tracks. Maybe try submitting that city again and see if it changes anything.</h3>";
+            weatherSectionEl.innerHTML = "<h3 class='alert'>Somebody just got diagnosed with skill issue. Could be you, could be me, could be the API I'm getting the data you requested from. It doesn't matter who has skill issue in the end though, as it is a disease that stops everything in its tracks. Maybe try submitting that city again and see if it changes anything.</h3>";
         }
     });
 };
@@ -111,19 +112,24 @@ var getWeatherData = function(geoData) {
 var displayWeatherData = function(geoData, weatherData) {
     var currentDate = new Date();
 
-    var currentWeatherEl = document.createElement("section")
-    currentWeatherEl.className = "current";
-    var headerEl = document.createElement("div");
-    headerEl.className = "header";
+    var weatherDataEl = document.createElement("div")
+    weatherDataEl.className = "card";
+    var currentWeatherEl = document.createElement("div");
+    currentWeatherEl.className = "card-content pb-3";
+    var currentHeaderEl = document.createElement("div");
+    currentHeaderEl.className = "media mb-3 is-align-items-center";
 
-    var headerTextEl = document.createElement("h2");
+    var imageEl = document.createElement("div");
+    imageEl.className = "media-left";
+    imageEl.innerHTML = "<figure class='image is-48x48'><img src='https://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + "@2x.png'></figure>";
+    currentHeaderEl.appendChild(imageEl);
+
+    var headerTextEl = document.createElement("div");
+    headerTextEl.className = "media-content title is-5";
     headerTextEl.textContent = geoData[0].name + " (" + String(currentDate.getMonth() + 1).padStart(2, '0') + "/" + String(currentDate.getDate()).padStart(2, '0') + "/" + currentDate.getFullYear() + ")";
-    headerEl.appendChild(headerTextEl);
+    currentHeaderEl.appendChild(headerTextEl);
 
-    var imageEl = document.createElement("img");
-    imageEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherData.current.weather[0].icon + "@2x.png");
-    headerEl.appendChild(imageEl);
-    currentWeatherEl.appendChild(headerEl);
+    currentWeatherEl.appendChild(currentHeaderEl);
 
     var currentTempEl = document.createElement("p");
     currentTempEl.textContent = "Temp: " + weatherData.current.temp + "°F";
@@ -155,46 +161,48 @@ var displayWeatherData = function(geoData, weatherData) {
     UVColorEl.textContent = weatherData.current.uvi;
     currentUVEl.appendChild(UVColorEl);
     currentWeatherEl.appendChild(currentUVEl);
-    weatherEl.appendChild(currentWeatherEl);
+    weatherDataEl.appendChild(currentWeatherEl);
 
-    var forecastHeaderEl = document.createElement("h3");
+    var futureWeatherEl = document.createElement("div");
+    futureWeatherEl.className = "card-content pt-3"
+    var forecastHeaderEl = document.createElement("div");
+    forecastHeaderEl.className = "title is-6";
     forecastHeaderEl.textContent = "5-Day Forecast:";
-    weatherEl.appendChild(forecastHeaderEl);
-
-    var forecastContainerEl = document.createElement("section");
-    forecastContainerEl.className = "forecast-container";
+    futureWeatherEl.appendChild(forecastHeaderEl);
 
     for(var i = 0; i < 5; i++) {
-        var forecastCardEl = document.createElement("div");
-        forecastCardEl.className = "forecast-card";
+        var forecastSubheaderEl = document.createElement("div");
+        forecastSubheaderEl.className = "media is-align-items-center";
+
+        var forecastImageEl = document.createElement("div");
+        forecastImageEl.className = "media-left";
+        forecastImageEl.innerHTML = "<figure class='image is-32x32'><img src='https://openweathermap.org/img/wn/" + weatherData.daily[i].weather[0].icon + "@2x.png'></figure>";
+        forecastSubheaderEl.appendChild(forecastImageEl);
 
         var forecastDate = new Date();
         forecastDate.setDate(forecastDate.getDate() + i + 1);
 
         var forecastDateEl = document.createElement("h4");
         forecastDateEl.textContent = String(forecastDate.getMonth() + 1).padStart(2, '0') + "/" + String(forecastDate.getDate()).padStart(2, '0') + "/" + forecastDate.getFullYear();
-        forecastCardEl.appendChild(forecastDateEl);
-
-        var forecastImageEl = document.createElement("img");
-        forecastImageEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherData.daily[i].weather[0].icon + "@2x.png");
-        forecastCardEl.appendChild(forecastImageEl);
+        forecastSubheaderEl.appendChild(forecastDateEl);
+        futureWeatherEl.appendChild(forecastSubheaderEl);
 
         var forecastTempEl = document.createElement("p");
         forecastTempEl.textContent = "Temp: " + weatherData.daily[i].temp.max + " °F";
-        forecastCardEl.appendChild(forecastTempEl);
+        futureWeatherEl.appendChild(forecastTempEl);
 
         var forecastWindEl = document.createElement("p");
         forecastWindEl.textContent = "Wind: " + weatherData.daily[i].wind_speed + " MPH";
-        forecastCardEl.appendChild(forecastWindEl);
+        futureWeatherEl.appendChild(forecastWindEl);
 
         var forecastHumidityEl = document.createElement("p");
+        forecastHumidityEl.className = "mb-4";
         forecastHumidityEl.textContent = "Humidity: " + weatherData.daily[i].humidity + " %";
-        forecastCardEl.appendChild(forecastHumidityEl);
-
-        forecastContainerEl.appendChild(forecastCardEl);
+        futureWeatherEl.appendChild(forecastHumidityEl);
     }
 
-    weatherEl.appendChild(forecastContainerEl);
+    weatherDataEl.appendChild(futureWeatherEl);
+    weatherSectionEl.appendChild(weatherDataEl);
 };
 
 /*var loadSearchHistory = function() {
